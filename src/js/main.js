f1422a95dffae3a53a70f39ea4fd1456e327b6a4
Paranoialol <drum0d3@gmail.com>
2021-07-04 22:26:21 +0300

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+import '../css/style.css';
+import '../index.html';
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
